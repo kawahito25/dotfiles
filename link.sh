9508b221028d7f5e5ff9ffe8b361fefd3b730f77
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-15 14:51:11 +0900

    add .hushlogin

--- link.sh
+++ link.sh
@@ -2,6 +2,7 @@ source ./.zshenv # for $DOTFILES_DIR
 
 ln -sf $DOTFILES_DIR/.zshenv ~/.zshenv
 ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
+ln -sf $DOTFILES_DIR/.hushlogin ~/.hushlogin
 ln -sf $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
 ln -sf $DOTFILES_DIR/.p10k.zsh ~/.p10k.zsh 
 ln -sf $DOTFILES_DIR/alacritty/ ~/.config/
