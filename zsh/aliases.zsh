96f329dca55bb0ac0b449846095102a119b14876
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-16 09:52:48 +0900

    tmux で lazygit をポップアップで表示する

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias cat='bat'
 alias ls='eza -F --icons=auto --hyperlink'
-alias lg='lazygit'
+alias lg='[ -n "$TMUX" ] && tmux display-popup -E -y40 -w 95% -h 95% -d "#{pane_current_path}" lazygit || lazygit'
 
