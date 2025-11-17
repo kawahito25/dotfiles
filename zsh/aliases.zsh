b0aa9e66a324fc3e26423f547438ac5dbb5ca272
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-18 01:18:16 +0900

    cat でページングをやめる

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
-alias cat='bat'
+alias cat='bat --paging=never'
 alias ls='eza -F --icons=auto --hyperlink'
 alias lg='[ -n "$TMUX" ] && tmux display-popup -E -w 100% -h 100% -d "#{pane_current_path}" lazygit || lazygit'
 alias nv='nvim'
