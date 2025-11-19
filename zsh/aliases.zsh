1e7369fc0c1045b7b18a9e318c768bd24cbac1fb
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-20 01:16:41 +0900

    tmux のウィンドウ切り替えようの alias を作成

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,5 @@ alias eza='LS_COLORS="" eza'
 alias ls='eza -F --icons=auto --hyperlink'
 alias lg='[ -n "$TMUX" ] && tmux display-popup -E -w100% -h100% -d "#{pane_current_path}" "lazygit" || lazygit'
 alias nv='nvim'
+alias tls='tmux list-windows -a -F "#{session_name}:#{window_index}:#{window_name}" | fzf --reverse --header "Select Window (Test)" | cut -d: -f3 | xargs -I@ tmux select-window -t @'
+
