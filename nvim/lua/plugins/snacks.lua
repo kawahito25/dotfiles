7ea80508985d79e3acebb13614b347148c0cf131
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-19 18:03:03 +0900

    snacks 有効化

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,4 +1,3 @@
-if true then return {} end
 
 -- @see https://github.com/folke/snacks.nvim/discussions/1701#discussioncomment-12934844
 local list_extend = function(where, what)
@@ -18,6 +17,7 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
+        enabled = true,
         sources = {
           grep = {
             -- 1. カスタム変数を定義 (デフォルトはケースインセンシティブ=false)
@@ -64,6 +64,22 @@ return {
           },
         },
       },
+    },
+    keys = { 
+      -- Top Pickers & Explorer
+      { "<leader><space>", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
+      { "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
+      { "<leader>/", function() Snacks.picker.grep() end, desc = "Grep" },
+      { "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
+      { "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
+      { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+      -- find
+      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+      { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
+      { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
+      { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+      { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
+      { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
     },
   },
 }
