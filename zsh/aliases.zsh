fbd92ad36c03c6688ecc13139616b8900b40823b
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-21 23:19:46 +0900

    git 関連のエイリアスを設定

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,16 @@
 alias cat='bat --paging=never'
 alias eza='LS_COLORS="" eza'
+
+alias gst='git status'
+alias gco='git checkout'
+alias gbr='git branch'
+alias gdf='git diff'
+alias gdfs='git diff --staged'
+alias glg='git log --oneline --graph --decorate'
+alias gad='git add .'
+alias gci='git commit'
+alias gcam='git commit --amend'
+
 alias ls='eza -F --icons=auto --hyperlink'
 alias lg='[ -n "$TMUX" ] && tmux display-popup -E -w100% -h100% -d "#{pane_current_path}" "lazygit" || lazygit'
 alias nv='nvim'
