2d0d47e7f460d44d1ae4006382c36c36d0c38458
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-22 15:18:58 +0900

    render-markdown.nvim の lazy load をやめる

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -1,6 +1,8 @@
 return {
     "MeanderingProgrammer/render-markdown.nvim",
-    ft = "markdown",
+    -- コメントアウト理由: nvim コマンドの引数としてマークダウンファイルを渡した際に、TODOリストの右側の文字が見切れる現象が発生したため
+    -- ft = "markdown",
+    -- event = "VeryLazy"
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
     opts = {},
 }
