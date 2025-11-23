3cbaeb7cfa21f02807e6ecb666e5dfe79be53650
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-23 12:42:08 +0900

    add blink.cmp settings

--- nvim/lua/plugins/blink.cmp.lua
+++ nvim/lua/plugins/blink.cmp.lua
@@ -1,6 +1,35 @@
+-- See Document: https://cmp.saghen.dev/
 return {
     'saghen/blink.cmp',
     version = '*',
     event = { "InsertEnter", "CmdlineEnter" },
-    opts = {},
+    opts = {
+        completion = {
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 200,
+            },
+            ghost_text = {
+                enabled = true
+            },
+            menu = {
+                draw = {
+                    columns = {
+                        { "kind_icon" },
+                        { "label",      "label_description", gap = 1 },
+                        { "source_name" }
+                    },
+                }
+            },
+        },
+        sources = {
+            default = { "lsp", "path", "snippets", "buffer" },
+            per_filetype = {
+                markdown = { "snippets", "path" },
+            },
+        },
+        keymap = {
+            preset = 'super-tab',
+        },
+    },
 }
