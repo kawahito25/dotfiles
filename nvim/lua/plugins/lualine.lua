37549f43349a4f1cd4cdc220e411f283088c9712
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-24 10:32:23 +0900

    gisigns と lualine の統合

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,5 +1,21 @@
+local function diff_source()
+    local gitsigns = vim.b.gitsigns_status_dict
+    if gitsigns then
+        return {
+            added = gitsigns.added,
+            modified = gitsigns.changed,
+            removed = gitsigns.removed
+        }
+    end
+end
+
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     event = "VeryLazy",
+    sections = {
+        lualine_b = {
+            { 'b:gitsigns_head', icon = '' },
+            { 'diff', source = diff_source }, },
+    }
 }
