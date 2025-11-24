5930070aebda0d29f6c8759e36e356926fbebb21
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-24 12:51:45 +0900

    sidekick.nvim 撤退

--- nvim/lua/plugins/blink.cmp.lua
+++ nvim/lua/plugins/blink.cmp.lua
@@ -30,23 +30,6 @@ return {
         },
         keymap = {
             preset = 'super-tab',
-            ["<Tab>"] = { -- @override super-tab
-                function(cmp)
-                    if cmp.snippet_active() then
-                        return cmp.accept()
-                    else
-                        return cmp.select_and_accept()
-                    end
-                end,
-                "snippet_forward",
-                function() -- sidekick next edit suggestion
-                    return require("sidekick").nes_jump_or_apply()
-                end,
-                -- function() -- if you are using Neovim's native inline completions
-                --     return vim.lsp.inline_completion.get()
-                -- end,
-                "fallback",
-            },
         },
     },
 }
