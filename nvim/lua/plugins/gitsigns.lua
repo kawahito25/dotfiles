45b8bd2f6fa1789986cafe4d3bfe3dd5e5ffb316
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-24 10:07:37 +0900

    gisigns.nvim の差分表示アイコンを変更

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -19,5 +19,21 @@ return {
             use_focus = true,
         },
         current_line_blame_formatter = "<author> (<author_time:%R>) - <summary> [<abbrev_sha>]",
+        signs = {
+            add          = { text = '+' },
+            change       = { text = '~' },
+            delete       = { text = '-' },
+            topdelete    = { text = '¯' },
+            changedelete = { text = '±' },
+            untracked    = { text = '?' },
+        },
+        signs_staged = {
+            add          = { text = '+' },
+            change       = { text = '~' },
+            delete       = { text = '-' },
+            topdelete    = { text = '¯' },
+            changedelete = { text = '±' },
+            untracked    = { text = '?' },
+        },
     },
 }
