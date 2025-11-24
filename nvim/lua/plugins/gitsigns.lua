15f4a1069f7c3cfe16962e8acddcc79853b54dce
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-24 10:18:17 +0900

    gisigns.nvim で行と行番号をハイライト

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -9,8 +9,8 @@ return {
     --   true  にすると行末などに blame 情報が常時表示される
     --   false にしておくと控えめな挙動（必要時に :Gitsigns blame_line など）
     opts = {
-        current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-        current_line_blame_opts = {
+        current_line_blame           = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+        current_line_blame_opts      = {
             virt_text = true,
             virt_text_pos = "eol",    -- 'eol' | 'overlay' | 'right_align'
             delay = 500,              -- default: 1000
@@ -19,7 +19,7 @@ return {
             use_focus = true,
         },
         current_line_blame_formatter = "<author> (<author_time:%R>) - <summary> [<abbrev_sha>]",
-        signs = {
+        signs                        = {
             add          = { text = '+' },
             change       = { text = '~' },
             delete       = { text = '-' },
@@ -27,7 +27,7 @@ return {
             changedelete = { text = '±' },
             untracked    = { text = '?' },
         },
-        signs_staged = {
+        signs_staged                 = {
             add          = { text = '+' },
             change       = { text = '~' },
             delete       = { text = '-' },
@@ -35,5 +35,8 @@ return {
             changedelete = { text = '±' },
             untracked    = { text = '?' },
         },
+        numhl                        = true,
+        linehl                       = true,
+        word_diff                    = true,
     },
 }
