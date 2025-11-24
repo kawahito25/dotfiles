c223fd3a0464994d7d806c02e4c87ecd8c2fa0ba
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-24 22:23:01 +0900

    add nvim-treesitter-textobjects

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,13 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false,
-    branch = "main",
-    build = ":TSUpdate",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        branch = "main",
+        build = ":TSUpdate",
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
+        event = "VeryLazy",
+    }
 }
