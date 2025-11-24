73b5d32a5a7e21e806c7d80f5bee10346e98667e
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-25 02:13:30 +0900

    nvim-ts-autotag 導入
    そもそも treesitter のハイライトが有効化されていなかったので対応

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -14,10 +14,29 @@ return {
                 enable = true
             }
         end,
+        config = function(_, opts)
+            vim.api.nvim_create_autocmd("FileType", {
+                group = vim.api.nvim_create_augroup("lazyvim_treesitter", { clear = true }),
+                callback = function(ev)
+                    pcall(vim.treesitter.start, ev.buf) -- highlighting
+                end
+            })
+        end
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
         branch = "main",
         event = "VeryLazy",
     },
+    {
+        "windwp/nvim-ts-autotag",
+        event = "VeryLazy",
+        opts = {
+            -- Defaults
+            enable_close = true,          -- Auto close tags
+            enable_rename = true,         -- Auto rename pairs of tags
+            enable_close_on_slash = false -- Auto close on trailing </
+        },
+    },
+    -- TODO: https://github.com/andymass/vim-matchup を入れる
 }
