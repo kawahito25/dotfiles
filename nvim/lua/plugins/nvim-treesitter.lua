a7a5e468ff9a8d3d017ac6903937e21ce2ad81a7
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-24 22:48:21 +0900

    endwise 導入

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -4,10 +4,18 @@ return {
         lazy = false,
         branch = "main",
         build = ":TSUpdate",
+        dependencies = {
+            "RRethy/nvim-treesitter-endwise",
+        },
+        opts = function(_, opts)
+            opts.endwise = {
+                enable = true
+            }
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter-textobjects",
         branch = "main",
         event = "VeryLazy",
-    }
+    },
 }
