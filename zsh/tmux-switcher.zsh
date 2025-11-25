73aac8f99cdc970a652aff5295e99d3e864402e1
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-26 02:09:28 +0900

    移動履歴をファイルに保存

--- zsh/tmux-switcher.zsh
+++ zsh/tmux-switcher.zsh
@@ -1,3 +1,19 @@
+#!/usr/bin/env zsh
+
+local history_num=20
+local history_file="$HOME/.dotfiles_tmux_history"
+
+echo "$history_file"
+touch "$history_file"
+
+function save_history() {
+    (echo $1; cat "$history_file" 2>/dev/null) \
+            | awk '!visited[$0]++ { print $0 }' \
+            | head -n $history_num > "${history_file}.tmp"
+
+    mv "${history_file}.tmp" "$history_file"
+}
+
 function tls() {
     # target_session=$(tmux list-sessions -F '#{session_name}' | fzf)
     windows=$(tmux list-windows -a -F "#{session_name}:#{window_index}:#{window_name}")
@@ -16,8 +32,10 @@ function tls() {
 
     if [ -z "$TMUX" ]; then
         tmux attach-session -t "$target_session:$target_window_idx"
+        save_history $selected
     else
         tmux switch-client -t "$target_session:$target_window_idx"
+        save_history $selected
     fi
 }
 
