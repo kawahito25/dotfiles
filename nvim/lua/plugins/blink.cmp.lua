9cde6df8621cabe3021094858735467156f5ad3f
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-27 00:46:29 +0900

    blink.cmp の keymap を修正する

--- nvim/lua/plugins/blink.cmp.lua
+++ nvim/lua/plugins/blink.cmp.lua
@@ -29,7 +29,15 @@ return {
             },
         },
         keymap = {
-            preset = 'super-tab',
+            -- note: ctrl + e または Esc で補完を無視して現在のテキストを保持する
+            ['<C-n>'] = { 'show', 'select_next', 'fallback' },
+            ['<C-p>'] = { 'show', 'select_prev', 'fallback' },
+            ['<Tab>'] = { 'select_next', 'fallback' },
+            ['<S-Tab>'] = { 'select_prev', 'fallback' },
+            ['<CR>'] = {
+                'accept',   -- 現在選択されている候補を決定して挿入
+                'fallback', -- 候補がない場合や決定できない場合は通常のEnterキーの動作（改行）を行う
+            },
         },
     },
 }
