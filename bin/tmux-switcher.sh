3cf91a087a05702921b2615e5c3d0e99e8f220ab
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-29 09:18:44 +0900

    tmux のステータスラインに、以前に移動したウィンドウを表示

--- bin/tmux-switcher.sh
+++ bin/tmux-switcher.sh
@@ -1,65 +1,17 @@
 #!/usr/bin/env zsh
 
-local history_num=20
-local history_file="$HOME/.dotfiles_tmux_history"
-
-touch "$history_file"
+local tmux_switch_history_path="$HOME/.dotfiles_tmux_history"
+touch "$tmux_switch_history_path"
 
 function save_history() {
-    (echo $1; cat "$history_file" 2>/dev/null) \
+    (echo $1; cat "$tmux_switch_history_path" 2>/dev/null) \
             | awk '!visited[$0]++ { print $0 }' \
-            | head -n $history_num > "${history_file}.tmp"
+            | head -n 20 > "${tmux_switch_history_path}.tmp"
 
-    mv "${history_file}.tmp" "$history_file"
+    mv "${tmux_switch_history_path}.tmp" "$tmux_switch_history_path"
 }
 
-# history_file のパスを変数に格納 (AWKに渡すため)
-local history_path="$history_file"
-
-# tmuxのウィンドウリストを取得
-windows=$(tmux list-windows -a -F "#{session_name}:#{window_index}:#{window_name}")
-
-if [ -z "$windows" ]; then
-    echo 'No Sessions'
-    return
-fi
-
-# 1. AWKを使って、各行にソートキーを付与する
-#    キーの形式: {順序インデックス}:{元の文字列}
-#    history_fileにない場合は、非常に大きなインデックス(99999999)を付与する
-selected_options=$(echo "$windows" | awk -v history_path="$history_path" '
-    # AWK実行前に一度だけ実行
-    BEGIN {
-        # 履歴ファイルから順序マップを読み込む
-        idx = 0;
-
-        # 1行目は現在いる場所なので、スキップする
-        if ((getline < history_path) > 0) {}
-
-        while ((getline line < history_path) > 0) {
-            order[line] = idx;
-            idx++;
-        }
-        close(history_path);
-    }
-
-    # 標準入力の各行($0)に対して実行
-    {
-        if ($0 in order) {
-            # 履歴にある場合: 順序インデックスを出力
-            key = order[$0];
-        } else {
-            # 履歴にない場合: 非常に大きなインデックスを付与
-            key = 99999999;
-        }
-
-        # 順序キーをタブ区切りで先頭に付与して出力
-        print key "\t" $0;
-    }
-' | sort -k1,1n -k2,2d | cut -f2-)
-
-# ソートされた結果を fzf に渡す
-selected=$(echo "$selected_options" | fzf --tmux)
+selected=$(tmux-list-fzf-options.sh | fzf --tmux)
 
 if [ -z "$selected" ]; then
     return
