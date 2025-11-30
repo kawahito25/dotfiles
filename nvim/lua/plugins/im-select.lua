d592651e0228ef41eec28263f71839015cc3ba48
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-30 23:56:11 +0900

    im-select.nvim で、Apple ではなく Google の IME をデフォルトで利用する

--- nvim/lua/plugins/im-select.lua
+++ nvim/lua/plugins/im-select.lua
@@ -2,6 +2,8 @@ return {
     "keaising/im-select.nvim",
     event = "InsertEnter",
     config = function()
-        require("im_select").setup({})
+        require("im_select").setup({
+            default_im_select = 'com.google.inputmethod.Japanese.Roman'
+        })
     end,
 }
