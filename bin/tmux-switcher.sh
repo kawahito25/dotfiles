261e6787d076247afeb07d8f37aca542702c8245
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-11-30 16:12:29 +0900

    移動履歴の保存を関数からスクリプトに変更

--- bin/tmux-switcher.sh
+++ bin/tmux-switcher.sh
@@ -3,14 +3,6 @@
 local tmux_switch_history_path="$HOME/.dotfiles_tmux_history"
 touch "$tmux_switch_history_path"
 
-function save_history() {
-    (echo $1; cat "$tmux_switch_history_path" 2>/dev/null) \
-            | awk '!visited[$0]++ { print $0 }' \
-            | head -n 20 > "${tmux_switch_history_path}.tmp"
-
-    mv "${tmux_switch_history_path}.tmp" "$tmux_switch_history_path"
-}
-
 selected=$(
     tmux-list-fzf-options.sh |
     awk -F: '{print " "$1"@ "$3"\t"$0}' |
@@ -28,10 +20,10 @@ target_window_idx=$(echo $selected | cut -d ':' -f 2)
 
 if [ -z "$TMUX" ]; then
     tmux attach-session -t "$target_session:$target_window_idx"
-    save_history $selected
+    tmux-save-switch-history.sh $selected
 else
     tmux switch-client -t "$target_session:$target_window_idx"
     tmux refresh-client -S
-    save_history $selected
+    tmux-save-switch-history.sh $selected
 fi
 
