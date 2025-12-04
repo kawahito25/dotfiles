6b813b59664ab7c8aac18279e67c2f643b5cc250
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-12-05 00:05:36 +0900

    vim-matchup 導入

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -48,6 +48,30 @@ return {
         opts = {
             enabled = true,
         }
+    },
+    {
+        'andymass/vim-matchup',
+        --[[ I do not recommend using alternative loading strategies such as event = 'VimEnter' or event = 'CursorMoved' as match-up already loads a minimal amount of code on start-up.
+        It may work, but if you run into issues, remove the event key as a first debugging step. ]]
+        event = { 'VimEnter', 'CursorMoved' },
+        init = function()
+            -- modify your configuration vars here
+            vim.g.matchup_treesitter_stopline = 500
+
+            -- or call the setup function provided as a helper. It defines the
+            -- configuration vars for you
+            require('match-up').setup({
+                treesitter = {
+                    stopline = 500
+                }
+            })
+        end,
+        -- or use the `opts` mechanism built into `lazy.nvim`. It calls
+        -- `require('match-up').setup` under the hood
+        opts = {
+            treesitter = {
+                stopline = 500,
+            }
+        }
     }
-    -- TODO: https://github.com/andymass/vim-matchup を入れる
 }
