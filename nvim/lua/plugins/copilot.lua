f01b59e03a3ce85eb9b31ad0d32ec62640635aa7
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-12-17 22:38:31 +0900

    CopilotChat.nvim 導入

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,13 +1,37 @@
 return {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    dependencies = {
-        "copilotlsp-nvim/copilot-lsp",
+    {
+        "zbirenbaum/copilot.lua",
+        event = "InsertEnter",
+        dependencies = {
+            "copilotlsp-nvim/copilot-lsp",
+        },
+        opts = {
+            -- blink-cmp-copilot の README を参照
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+            copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/25.1.0/bin/node", -- Node.js version must be > 22
+        }
     },
-    opts = {
-        -- blink-cmp-copilot の README を参照
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-        copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/25.1.0/bin/node", -- Node.js version must be > 22
-    }
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", branch = "master" },
+        },
+        build = "make tiktoken",
+        cmd = "CopilotChat", -- lazy-load on a command
+        opts = { model = 'gpt-4.1' },
+        keys = {
+            {
+                "<leader>hR",
+                function()
+                    require("CopilotChat").ask(
+                        "バグの可能性がないか、よりシンプルで可読性の高いコードに変更できないか、という観点で日本語でコードレビューして。", {
+                            model = "gpt-4.1",
+                            sticky = { "#buffer", "#gitdiff:staged" }
+                        })
+                end,
+                desc = "AI Review"
+            },
+        }
+    },
 }
