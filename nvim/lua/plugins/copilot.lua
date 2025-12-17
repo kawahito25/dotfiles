d2c8b9d6579ceb29bebcbe24e8d94d7fbfc59203
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-12-17 21:52:42 +0900

    node version が 21以下のプロジェクトでも動作するように修正

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -8,5 +8,6 @@ return {
         -- blink-cmp-copilot の README を参照
         suggestion = { enabled = false },
         panel = { enabled = false },
+        copilot_node_command = vim.fn.expand("$HOME") .. "/.asdf/installs/nodejs/25.1.0/bin/node", -- Node.js version must be > 22
     }
 }
