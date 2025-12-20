c3c6c8168c5d593ccdf3df1ab380194117c33f6b
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-12-20 21:17:32 +0900

    which-key に delay を追加

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -2,6 +2,7 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
+        delay = 400,
         -- presets: https://github.com/folke/which-key.nvim/blob/main/lua/which-key/presets.lua
         win = {
             width = math.huge,
