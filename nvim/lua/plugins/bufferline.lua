d5d698cbb8519a7c8c9e895e2d0594839ca082a7
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-12-20 12:33:07 +0900

    bufferline.nvim を無効化

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,18 +1,20 @@
-return {
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    version = "*",
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    opts = {
-        options = {
-            offsets = {
-                {
-                    -- @see https://github.com/folke/snacks.nvim/discussions/1340
-                    filetype = 'snacks_layout_box',
-                    text = '',
-                    separator = true,
-                }
-            },
-        }
-    },
-}
+return {}
+
+-- return {
+--     "akinsho/bufferline.nvim",
+--     event = "VeryLazy",
+--     version = "*",
+--     dependencies = 'nvim-tree/nvim-web-devicons',
+--     opts = {
+--         options = {
+--             offsets = {
+--                 {
+--                     -- @see https://github.com/folke/snacks.nvim/discussions/1340
+--                     filetype = 'snacks_layout_box',
+--                     text = '',
+--                     separator = true,
+--                 }
+--             },
+--         }
+--     },
+-- }
