a44f9f55049d2f36f176606b4a406f55d385cc89
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-12-20 13:26:14 +0900

    lazygit 撤退

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -19,7 +19,6 @@ alias gpu='git push origin HEAD'
 
 alias ls='eza -F --icons=auto'
 alias tree='ls -T'
-alias lg='[ -n "$TMUX" ] && tmux display-popup -E -w100% -h100% -d "#{pane_current_path}" "lazygit" || lazygit'
 alias nv='nvim'
 
 
--- zsh/base.zsh
+++ zsh/base.zsh
@@ -9,16 +9,13 @@ export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/opt/homebrew/share/zsh-syntax-highlightin
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 # brew install ripgrep
-export RIPGREP_CONFIG_PATH=$DOTFILES_DIR/.ripgreprc 
+export RIPGREP_CONFIG_PATH=$DOTFILES_DIR/.ripgreprc
 
 # brew install asdf
 export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 
 # brew install eza
 export EZA_CONFIG_DIR="$HOME/.config/eza"
-
-# brew install lazygit
-export LG_CONFIG_FILE="$(lazygit --print-config-dir)/config.yml,$DOTFILES_DIR/submodules/catppuccin/lazygit/themes-mergable/mocha/mauve.yml"
 
 # add path for dotfiles scripts
 export PATH="$DOTFILES_DIR/bin:$PATH"
