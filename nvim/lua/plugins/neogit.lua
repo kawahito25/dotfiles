577dbe1f95dda18fa1ed37e13cd1d650f94b47c0
kawahito <67470479+kawahito25@users.noreply.github.com>
2025-12-24 00:38:38 +0900

    feat(neogit): add keymaps for git log and -S search in visual mode
    Adds <leader>gl for git log and visual <leader>gl for git log -S using the selected text.

--- nvim/lua/plugins/neogit.lua
+++ nvim/lua/plugins/neogit.lua
@@ -17,6 +17,21 @@ return {
         },
     },
     keys = {
-        { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" }
+        { "<leader>gg", "<cmd>Neogit<cr>",                                               desc = "Open Neogit" },
+        { "<leader>gc", function() require('neogit').open({ "commit" }) end,             desc = "Git Commit (Neogit)" },
+        { "<leader>gl", function() require('neogit').action("log", "log_current")() end, desc = "Git Log (Neogit)" },
+        {
+            "<leader>gl",
+            function()
+                local region = vim.fn.getregion(vim.fn.getpos("v"), vim.fn.getpos("."), { type = vim.fn.mode() })
+                local selection = table.concat(region, "\n")
+
+                if selection == "" then return end
+                local action = require('neogit').action("log", "log_current", { "-S", selection })
+                action()
+            end,
+            desc = "Git Log -S (Neogit)",
+            mode = "v"
+        },
     },
 }
