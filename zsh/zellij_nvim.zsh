ca787b4b53f8cac038333715a81213aa00a43131
kawahito <67470479+kawahito25@users.noreply.github.com>
2026-01-01 16:27:59 +0900

    feat(zsh): add zellij navigation keybindings for nvim integration
    Add explicit Zsh keybindings for Zellij navigation to resolve vim-zellij-navigator issues when nvim runs as a background process.

---
+++ zsh/zellij_nvim.zsh
@@ -0,0 +1,20 @@
+# nvim がバックグラウンドプロセスにいるときに、vim-zellij-navigator が動かないので明示的にシェルのキーバインディングを追記
+# GitHub Issue: https://github.com/hiasr/vim-zellij-navigator/issues/24
+
+# Zellij 移動用関数を定義
+function zellij-move-left()  { zellij action move-focus-or-tab left  }
+function zellij-move-right() { zellij action move-focus-or-tab right }
+function zellij-move-down()  { zellij action move-focus down  }
+function zellij-move-up()    { zellij action move-focus up    }
+
+# ZLE (Zsh Line Editor) ウィジェットとして登録
+zle -N zellij-move-left
+zle -N zellij-move-right
+zle -N zellij-move-down
+zle -N zellij-move-up
+
+# キーバインドの設定 (Ctrl + h, l, j, k)
+bindkey '^H' zellij-move-left
+bindkey '^L' zellij-move-right
+bindkey '^J' zellij-move-down
+bindkey '^K' zellij-move-up
