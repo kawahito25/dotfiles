78d48fc9245f3988f702f844f1565f76564921d6
kawahito <67470479+kawahito25@users.noreply.github.com>
2026-01-03 00:36:25 +0900

    feat(marks): enable default mappings
    Default mappings are now enabled for marks.nvim.
    Note: On Japanese keyboards, marks 0-9 may not work as expected.

--- nvim/lua/plugins/marks.lua
+++ nvim/lua/plugins/marks.lua
@@ -1,5 +1,5 @@
 return {
     "chentoast/marks.nvim",
     event = "VeryLazy",
-    opts = { default_mappings = false },
+    opts = { default_mappings = true }, -- WARN: 日本語キーボードで、0 ~ 9 のマークが動作しない
 }
