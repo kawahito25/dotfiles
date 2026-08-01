61c1296b4d4c7dde85e8174ebf4269087d5324ab
kawahito <67470479+kawahito25@users.noreply.github.com>
2026-08-01 18:45:18 +0900

    feat(vim): toggle markdown preview with leader + um

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -2,5 +2,17 @@ return {
     "MeanderingProgrammer/render-markdown.nvim",
     ft = "markdown",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-    opts = {},
+    opts = {
+        enabled = false,
+        anti_conceal = { enabled = false },
+    },
+    keys = {
+        {
+            "<leader>um",
+            function()
+                require('render-markdown').buf_toggle()
+            end,
+            desc = "Toggle Markdown Preview",
+        },
+    },
 }
