558060df419c531e5fc52fd720eccf891a4b8352
kawahito <67470479+kawahito25@users.noreply.github.com>
2026-08-07 23:25:10 +0900

    refactor(nvim): lsp search

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -225,14 +225,25 @@ return {
             desc = 'Search Changes',
         },
         -- LSP
-        { "gd",         function() Snacks.picker.lsp_definitions() end,                 desc = "Goto Definition" },
-        { "gD",         function() Snacks.picker.lsp_declarations() end,                desc = "Goto Declaration" },
-        { "gr",         function() Snacks.picker.lsp_references() end,                  nowait = true,                  desc = "References" },
-        { "gI",         function() Snacks.picker.lsp_implementations() end,             desc = "Goto Implementation" },
-        { "gy",         function() Snacks.picker.lsp_type_definitions() end,            desc = "Goto T[y]pe Definition" },
-        { "gai",        function() Snacks.picker.lsp_incoming_calls() end,              desc = "C[a]lls Incoming" },
-        { "gao",        function() Snacks.picker.lsp_outgoing_calls() end,              desc = "C[a]lls Outgoing" },
-        { "<leader>ss", function() Snacks.picker.lsp_symbols({ layout = "right" }) end, desc = "LSP Symbols" },
+        { "gd",  function() Snacks.picker.lsp_definitions() end,      desc = "Goto Definition" },
+        { "gD",  function() Snacks.picker.lsp_declarations() end,     desc = "Goto Declaration" },
+        { "gr",  function() Snacks.picker.lsp_references() end,       nowait = true,                  desc = "References" },
+        { "gI",  function() Snacks.picker.lsp_implementations() end,  desc = "Goto Implementation" },
+        { "gy",  function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+        { "gai", function() Snacks.picker.lsp_incoming_calls() end,   desc = "C[a]lls Incoming" },
+        { "gao", function() Snacks.picker.lsp_outgoing_calls() end,   desc = "C[a]lls Outgoing" },
+        {
+            "<leader>ss",
+            function()
+                Snacks.picker.lsp_symbols({
+                    layout = "right",
+                    filter = {
+                        ruby = { "Module", "Class", "Constructor", "Method", "Function" }
+                    }
+                })
+            end,
+            desc = "LSP Symbols"
+        },
         -- open
         {
             "<leader>ogf",
